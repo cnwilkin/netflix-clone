b13df0bcc72c0e3754e0797f8a602bd8274c3c83
Chad Wilkin <cnw1of4@gmail.com>
2020-07-25 23:34:22 -0400

    Save

---
+++ src/Row.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+
+function Row({ title }) {
+    const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+
+    }, []);
+    
+    return (
+        <div>
+            <h2>{title}</h2>
+            
+            {/* container -> posters */}
+        </div>
+    )
+}
+
+export default Row
